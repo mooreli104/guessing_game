e1a49cfa914338f95cdc5727f230f878fadea141
mooreli104 <102637376+mooreli104@users.noreply.github.com>
2025-01-20 19:28:18 -0500

    Find alternative titles

--- guessing-game/src/app.py
+++ guessing-game/src/app.py
@@ -14,13 +14,26 @@ CLIENT_SECRET = os.getenv('CLIENT_SECRET')
 # Gets the most popular anime from MAL 
 # Offset will offset rankings, so instead of the 1, 2, 3, most popular anime 
 # offset of 100 returns the 101, 102, 103 most popular anime
-url = 'https://api.myanimelist.net/v2/anime/ranking?ranking_type=all&limit=4&offset=100'
+url = 'https://api.myanimelist.net/v2/anime/ranking?ranking_type=all&limit=4&offset=1000'
 
 header = {
     'X-MAL-CLIENT-ID': CLIENT_ID
 }
+
+
+url2 = 'https://api.myanimelist.net/v2/anime/30230?fields=alternative_titles'
+object = requests.get(url2, headers =header).json()
+print(object['alternative_titles'])
+
+
 response = requests.get(url,headers=header).json()
 for x in response['data']:
+    anime_id = x['node']['id']
+    url2 = f'https://api.myanimelist.net/v2/anime/{anime_id}'
+    object = requests.get(url2, headers =header).json()
+    if 'alternative_titles' in object:
+        print(object['alternative_titles'])
+
     print('Rank: ' + str(x['ranking']['rank']) + '\n' + 'Title: ' + x['node']['title'] + '\n' + 'Image url: ' + x['node']['main_picture']['large']+'\n') 
 
 
