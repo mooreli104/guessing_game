a7ad616db2f6ec48e61b745bb54d5c201ffa19d3
mooreli104 <102637376+mooreli104@users.noreply.github.com>
2025-01-26 18:59:51 -0500

    Create player boxes

---
+++ guessing-game/src/components/Player.vue
@@ -0,0 +1,29 @@
+<script setup>
+   const props = defineProps(['username'])
+</script>
+
+<template>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0&icon_names=person" />
+
+    <div id = "player">
+        <span class="material-symbols-outlined icon">person</span>
+        <div v-if="username">{{$route.params.username}}</div>
+        <div v-else>Empty</div>
+    </div>
+</template>
+
+<style scoped>
+#player{
+    display: flex;
+    align-items:center;
+    justify-content: space-between;
+    width: 20vh;
+    height: 5vh;
+    border: 2px solid red;
+}
+
+.icon{
+    margin-right: 2vh;
+}
+
+</style>
