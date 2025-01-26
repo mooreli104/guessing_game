ba374deeb1c90424f91f0a8b0e3944d2dd7f0c7e
mooreli104 <102637376+mooreli104@users.noreply.github.com>
2025-01-26 18:59:13 -0500

    Create button to route to lobby

---
+++ guessing-game/src/components/Home.vue
@@ -0,0 +1,25 @@
+<script setup>
+
+  import { useRouter } from 'vue-router'
+  import { ref } from 'vue'
+  
+  // Uses router from main.js to push Lobby page
+  const router = useRouter()  
+  const username = ref(''); //username is linked with v-model = "username"
+  const goToLobby = () => {
+    router.push({ name: 'lobby', params: { username: username.value } })
+  }
+
+</script>
+
+<template>
+
+  <input type="text" v-model = "username" placeholder="Enter a nickname!">
+  <button type = "submit" value = "Start" @click = "goToLobby">Start</button>
+
+</template>
+
+<style scoped>
+
+
+</style>
