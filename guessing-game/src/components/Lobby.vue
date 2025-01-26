fdffa0f9ac9d8839eacd75b8cf94e3ad6a766d44
mooreli104 <102637376+mooreli104@users.noreply.github.com>
2025-01-26 18:59:39 -0500

    Create routes to home and lobby Add players

---
+++ guessing-game/src/components/Lobby.vue
@@ -0,0 +1,34 @@
+<script setup>
+import Player from './Player.vue'
+
+import { useRouter } from 'vue-router'
+  
+  // Uses router from main.js to push Lobby page
+  const router = useRouter()
+  const goToHome = () => {
+    router.push('/')
+  }
+  const goToGame = () => {
+    router.push('/game')
+  }
+
+</script>
+
+<template>
+    <button @click = "goToHome">HOME</button>
+
+    <div>
+    <Player username = 'username'></Player>
+    <Player></Player>
+    <Player></Player>
+    <Player></Player>
+    <Player></Player>
+    </div>
+    
+    <button @click = "goToGame">Start Game</button>
+</template>
+
+<style scoped>
+
+
+</style>
