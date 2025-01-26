15678f70e3612c3dd0f3344850f8e12398b5c4d7
mooreli104 <102637376+mooreli104@users.noreply.github.com>
2025-01-26 18:58:24 -0500

    Create game lobby and get images
    Co-authored-by: Andy Hou <24houand@hawken.edu>

---
+++ guessing-game/src/components/Game.vue
@@ -0,0 +1,25 @@
+<script setup>
+
+//Get anime image 
+    fetch('http://127.0.0.1:5000')
+        .then(response => response.json())
+        .then(data => { 
+            for (let index = 0; index < data.length; index++) {
+                const image_url = data[index]['image_url']
+                var image = document.createElement("img");
+                var imageParent = document.body;
+                image.src = image_url;       
+                imageParent.appendChild(image);
+            }
+        }); 
+
+</script>
+
+
+<template>
+</template>
+
+
+<styles scoped>
+
+</styles>
