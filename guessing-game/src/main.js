94f8e4cc746477580331fa3ef47637ba8e7eadab
mooreli104 <102637376+mooreli104@users.noreply.github.com>
2025-01-26 19:00:03 -0500

    Create routes

--- guessing-game/src/main.js
+++ guessing-game/src/main.js
@@ -1,6 +1,26 @@
 import './assets/main.css'
 
-import { createApp } from 'vue'
-import HomePage from './App.vue'
 
-createApp(HomePage).mount('#homepage')
+//Import VueRouter dependents
+import { createMemoryHistory, createRouter } from 'vue-router'
+import { createApp } from 'vue'
+
+//Import Vue components
+import App from './App.vue'
+import HomePage from './components/Home.vue'
+import LobbyPage from './components/Lobby.vue'
+import GamePage from './components/Game.vue'
+
+//Define routes
+const routes = [
+    { path: '/', component: HomePage },
+    { path: '/lobby/:username', name: 'lobby', component: LobbyPage }, // :username means that username is a parameter
+    { path: '/game', name: 'game', component: GamePage },
+  ]
+  
+  const router = createRouter({
+    history: createMemoryHistory(),
+    routes,
+  })
+
+createApp(App).use(router).mount('#homepage')
