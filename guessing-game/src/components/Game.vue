29c2138ddc581de160adc8ae06d07d7acbddf14c
mooreli104 <102637376+mooreli104@users.noreply.github.com>
2025-02-03 22:23:28 -0500

    Remove API call

--- guessing-game/src/components/Game.vue
+++ guessing-game/src/components/Game.vue
@@ -1,25 +1,10 @@
 <script setup>
 import { useRouter } from 'vue-router'
 import { players } from '../websocket'
+import { img_url } from '../websocket'
 import Player from './Player.vue'
 
 const router = useRouter()
-
-//Get anime image 
-fetch('http://127.0.0.1:5000')
-    .then(response => response.json())
-    .then(data => {
-        for (let index = 0; index < data.length; index++) {
-            const image_url = data[index]['image_url']
-            var image = document.createElement("img");
-            var imageParent = document.getElementById("images");
-            image.src = image_url
-            image.width = '239.25'
-            image.height = '337.5'
-            imageParent.appendChild(image);
-
-        }
-    });
 
 const leave = () => {
     router.push('/')
@@ -28,28 +13,42 @@ const leave = () => {
 
 
 <template>
-    <div id="images"></div>
     <button @click="leave">Leave</button>
+    <div class="container">
+
+    <div class="guess">
+    <div id="images"><img :src="img_url" alt="anime_image"></div>
     <input type="text" v-model="username" placeholder="Guess!">
+</div>
 
     <div id="players">
         <ul v-for="item in players">
             <Player :username=item.name></Player>
         </ul>
     </div>
-
-
+</div>
 </template>
 
 
 <style scoped>
+.container{
+    display: flex;
+    
+}
 #players {
     display: flex;
     flex-direction: column;
 }
 
+
+
 ul {
     list-style: none;
     padding: 0;
+}
+
+image {
+    width: '239.25';
+    height: '337.5';
 }
 </style>
