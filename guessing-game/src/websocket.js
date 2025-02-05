e2ba8494420a15c145287fed2bdc1fb94048a6b9
mooreli104 <102637376+mooreli104@users.noreply.github.com>
2025-02-04 21:49:04 -0500

    Get opponents

--- guessing-game/src/websocket.js
+++ guessing-game/src/websocket.js
@@ -12,17 +12,22 @@ export let state;
 export let players = ref([])
 export let img_url = ref("")
 export let rank = ref()
+export let opponents = ref([])
 
 socket.on("connected", (server_players) => {
   players.value = server_players
+  opponents.value = server_players.filter((player) => player.id !== socket.id)
+
 })
 
 socket.on('disconnected', (server_players) => {
   players.value = server_players
+  opponents.value = server_players.filter((player) => player.id !== socket.id)
 })
 
 socket.on('start-game', (playing_players) => {
   players.value = playing_players
+  opponents.value = playing_players.filter((player) => player.id !== socket.id)
 })
 
 socket.on("send-to-game", (object) => {
